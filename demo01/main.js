fe35c186e1eeae679de8395aa2df4ccafabeb57c
AatuManninen <63038287+AatuManninen@users.noreply.github.com>
2020-08-31 12:17:42 +0300

    Update main.js

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"); // muuttuja  (http moduuli)
+const url = require("url");   // muuttuja (url moduuli)
 
-const portti = 3001;
+const portti = 3001; // portin numero muuttujana
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { //serverin tekeminen 
 
-    if (req.url != "/favicon.ico") {
+    if (req.url != "/favicon.ico") { // jonkin sortin if lause
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query; // muuttuja josta parsitaan jtn
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";// query string muuttuja, joka on aina tuntematon
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });< // muutetaan sisältö html:lään
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); // ensimmäinen teksti
+        res.write(`<h2>Heippa, ${nimi}!</h2>`);//toinen teksti
+        res.end();//lopetus 
 
     }
 
 });
 
-serveri.listen(portti, () => {
+serveri.listen(portti, () => { //serverin käynnistäminen 
     console.log(`Palvelin käynnistyi porttiin ${portti}`);   
 });
